5e4a38f1d792da019298557ef1eb1cdc8b0fe95e
ClloudyTears <79748992+ClloudyTears@users.noreply.github.com>
2021-02-27 19:37:00 +0800

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,1 +1,19 @@
-Not yet
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+  name: `ping`,
+  category: `info`,
+  descriptiong: `API ping`,
+
+  /**
+   * @param {Client} client 
+   * @param {Message} message 
+   * @param {String[]} args
+   */
+
+  run : async(client, message, args) => {
+    const embed = new MessageEmbed()
+    .setTitle('Pong!')
+    .setDescription(`Websocket ping is ${client.ws.ping}MS\nMessage edit ping is ${Math.floor(message.createdAt - message.createdAt)}MS!`)
+    await message.channel.send(embed)
+  }
+}
